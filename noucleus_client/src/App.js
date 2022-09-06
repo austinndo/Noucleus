d47a76f2ba0ac4b9b4e0cb3da62a00b8c5da2104
Austin Do <austinndo@gmail.com>
2022-09-05 21:12:01 -0700

    changed public/index.html and trying out api/axios call

--- noucleus_client/src/App.js
+++ noucleus_client/src/App.js
@@ -1,7 +1,31 @@
 import './App.css'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+const URL = 'http://localhost:8000/'
 
 function App() {
-  return <div className="App"></div>
+  const [genes, setGenes] = useState([])
+
+  useEffect(() => {
+    const getGenes = async () => {
+      let res = await axios.get(`${URL}/genes`)
+      setGenes(res.data)
+      console.log(res.data)
+    }
+    getGenes()
+  }, [])
+
+  return (
+    <div className="App">
+      {genes.map((gene) => (
+        <div key={gene.id}>
+          <h2>{gene.name}</h2>
+          <h2>{gene.function}</h2>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default App
