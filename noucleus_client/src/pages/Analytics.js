ff20ac6cc15907efbb7a4b359542c935f127aa99
Austin Do <austinndo@gmail.com>
2022-09-07 22:47:44 -0700

    Added first data viz for percent GC

--- noucleus_client/src/pages/Analytics.js
+++ noucleus_client/src/pages/Analytics.js
@@ -1,10 +1,32 @@
 import { useEffect } from 'react'
+import { ProgressCircular } from 'react-rainbow-components'
 
-const Analytics = ({ setSidebarPage }) => {
+const Analytics = ({ setSidebarPage, guides }) => {
   useEffect(() => {
     setSidebarPage('Analytics')
   }, [])
-  return <h2>Analytics Page</h2>
+
+  if (guides) {
+    return (
+      <div>
+        <h2>Analytics Page</h2>
+        {guides.map((guide) => (
+          <div className="AnalyticsCard" key={guide.id}>
+            <h2>Designer: {guide.user}</h2>
+            <ProgressCircular value={guide.percent_gc} variant="success" />
+            <h1 className="rainbow-font-size-heading_small rainbow-color_gray-3">
+              Percent GC
+            </h1>
+
+            <h2>Efficiency: {guide.efficieny}</h2>
+            <h2>Percent GC: {guide.percent_gc}</h2>
+          </div>
+        ))}
+      </div>
+    )
+  } else {
+    return <h2>Guide Analytics loading ...</h2>
+  }
 }
 
 export default Analytics
